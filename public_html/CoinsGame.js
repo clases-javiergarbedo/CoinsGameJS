dee820a7ab4f749605eb8168805905cd0ad83a81
Javier García Escobedo <javier@iMac-de-Javier.local>
2015-02-09 22:45:40 +0100

    Instance methods

--- public_html/CoinsGame.js
+++ public_html/CoinsGame.js
@@ -16,6 +16,10 @@ function CoinsGame() {
     this.MOVEMENT_VALIDATION_UNDEFINED = 2;
     
     
+    /************ 
+    * Constructor initializations 
+    ************/   
+    
     // Init gameboard with: "**** ####"
     for(var i = 0; i < 4; i++) {
         this.gameboard[i] = "*";
@@ -24,88 +28,90 @@ function CoinsGame() {
     for(var i = 5; i < 9; i++) {
         this.gameboard[i] = "#";
     } 
-};
+
     
-/************ 
-* Methods declaration
-************/
+    /************ 
+    * Methods declaration
+    ************/
 
-CoinsGame.prototype.getGameboard = function() {
-    var result = "<pre>";
-    for(var i = 0; i < this.gameboard.length; i++) {
-        result += i;
-    }
-    result += "<br>";
-    for(var i = 0; i < this.gameboard.length; i++) {
-        result += this.gameboard[i];
-    }
-    result += "</pre>";
-    return result;
-};
-
-CoinsGame.prototype.getMovementValidation = function(movementPosition) {
-    console.log("Movement validation for position: " + movementPosition);
-    if(typeof movementPosition === "undefined") {
-        console.log("Movement position undefined");
-        return this.MOVEMENT_VALIDATION_UNDEFINED;
-    } else {
-        if(movementPosition < 0 || movementPosition >= this.gameboard.length) {
-            console.log("Movement attempt out of range");
-            return this.MOVEMENT_VALIDATION_OUT_OF_RANGE;
-        } else {
-            console.log("Movement correct");
-            return this.MOVEMENT_VALIDATION_OK;
+    this.getGameboard = function() {
+        var result = "<pre>";
+        for(var i = 0; i < this.gameboard.length; i++) {
+            result += i;
         }
-    }
-};
+        result += "<br>";
+        for(var i = 0; i < this.gameboard.length; i++) {
+            result += this.gameboard[i];
+        }
+        result += "</pre>";
+        return result;
+    };
 
-CoinsGame.prototype.moveCoin = function(movementPosition) {
-    var movementPositionInt = parseInt(movementPosition);
-    switch(this.gameboard[movementPositionInt]) {
-        case "*":
-            // Move to right if posible
-            console.log("In +1:" + this.gameboard[movementPositionInt + 1] + "!");
-            if(this.gameboard[movementPositionInt + 1] === " ") {
-                this.gameboard[movementPositionInt + 1] = "*";
-                this.gameboard[movementPositionInt] = " ";
-                console.log("Move 1");
-                return true;
-            } else if(this.gameboard[movementPositionInt + 1] === "#" && 
-                    this.gameboard[movementPositionInt + 2] === " ") {
-                this.gameboard[movementPositionInt + 2] = "*";
-                this.gameboard[movementPositionInt] = " ";
-                console.log("Move 2");
-                return true;
+    this.getMovementValidation = function(movementPosition) {
+        console.log("Movement validation for position: " + movementPosition);
+        if(typeof movementPosition === "undefined") {
+            console.log("Movement position undefined");
+            return this.MOVEMENT_VALIDATION_UNDEFINED;
+        } else {
+            if(movementPosition < 0 || movementPosition >= this.gameboard.length) {
+                console.log("Movement attempt out of range");
+                return this.MOVEMENT_VALIDATION_OUT_OF_RANGE;
             } else {
-                console.log("Move 0");
-                return false;
+                console.log("Movement correct");
+                return this.MOVEMENT_VALIDATION_OK;
             }
-            break;
-        case "#":
-            // Move to left if posible
-            if(this.gameboard[movementPositionInt - 1] === " ") {
-                this.gameboard[movementPositionInt - 1] = "#";
-                this.gameboard[movementPositionInt] = " ";
-                console.log("Move -1");
-                return true;
-            } else if(this.gameboard[movementPositionInt - 1] === "*" && 
-                    this.gameboard[movementPositionInt - 2] === " ") {
-                this.gameboard[movementPositionInt - 2] = "#";
-                this.gameboard[movementPositionInt] = " ";
-                console.log("Move -2");
-                return true;
-            } else {
-                console.log("Move 0");
+        }
+    };
+
+    this.moveCoin = function(movementPosition) {
+        var movementPositionInt = parseInt(movementPosition);
+        switch(this.gameboard[movementPositionInt]) {
+            case "*":
+                // Move to right if posible
+                console.log("In +1:" + this.gameboard[movementPositionInt + 1] + "!");
+                if(this.gameboard[movementPositionInt + 1] === " ") {
+                    this.gameboard[movementPositionInt + 1] = "*";
+                    this.gameboard[movementPositionInt] = " ";
+                    console.log("Move 1");
+                    return true;
+                } else if(this.gameboard[movementPositionInt + 1] === "#" && 
+                        this.gameboard[movementPositionInt + 2] === " ") {
+                    this.gameboard[movementPositionInt + 2] = "*";
+                    this.gameboard[movementPositionInt] = " ";
+                    console.log("Move 2");
+                    return true;
+                } else {
+                    console.log("Move 0");
+                    return false;
+                }
+                break;
+            case "#":
+                // Move to left if posible
+                if(this.gameboard[movementPositionInt - 1] === " ") {
+                    this.gameboard[movementPositionInt - 1] = "#";
+                    this.gameboard[movementPositionInt] = " ";
+                    console.log("Move -1");
+                    return true;
+                } else if(this.gameboard[movementPositionInt - 1] === "*" && 
+                        this.gameboard[movementPositionInt - 2] === " ") {
+                    this.gameboard[movementPositionInt - 2] = "#";
+                    this.gameboard[movementPositionInt] = " ";
+                    console.log("Move -2");
+                    return true;
+                } else {
+                    console.log("Move 0");
+                    return false;
+                }
+                break;
+            case " ":
+                // Don´t move
+                console.log("Don't move");
                 return false;
-            }
-            break;
-        case " ":
-            // Don´t move
-            console.log("Don't move");
-            return false;
-            break;
-    }
-    
+                break;
+        }
+
+    };
+
 };
 
 
